95a610a93f2f1f7406cede413ed9dfec6f8dbf6e
German Monzon <german.monzon@wnco.com>
2020-04-26 06:00:06 -0300

    Bit and Storybook base

---
+++ React/Bit/bit-example/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import ProductList from '@bit/german969.ui-components.product-list';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
